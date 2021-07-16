beedd9d5ba751a7d52e6933af586b3d893569252
kuroko1t <kurosawa.yk@gmail.com>
2021-07-17 03:15:35 +0900

    update

--- main.go
+++ main.go
@@ -11,7 +11,14 @@ import (
 
 	"github.com/fatih/color"
 	"github.com/go-git/go-git/v5"
+	//"github.com/djherbis/times"
 )
+
+var detailFlag bool
+
+func init() {
+  flag.BoolVar(&detailFlag, "l", false, "show detail")
+}
 
 func main() {
 	flag.Parse()
@@ -23,7 +30,7 @@ func main() {
 	}
 }
 
-func checkGitBranch(dirname string) bool {
+func checkGitBranch(file fs.FileInfo, dirname string) bool {
 	r, err := git.PlainOpen(dirname)
 	if err != nil {
 		return true
@@ -32,41 +39,70 @@ func checkGitBranch(dirname string) bool {
 	if err != nil {
 		return true
 	}
-	color.HiBlue(dirname + "(" + ref.Name().Short() + ")")
+	printString := ""
+	if detailFlag {
+		fileModeString := fmt.Sprintf("%v", file.Mode())
+		fileModeString += fmt.Sprintf(" %v", file.Size())
+		fileModeString += fmt.Sprintf(" %v ", file.ModTime().Format("2006/01/02 15:04"))
+		printString = fileModeString + " " + dirname + "(" + ref.Name().Short() + ")"
+	} else {
+		printString = dirname + "(" + ref.Name().Short() + ")"
+	}
+	color.HiBlue(printString)
 	return false
 }
 
-func checkPrint(filename string) {
+func checkExt(file fs.FileInfo, filename string) {
+	printString := ""
+	if detailFlag {
+		fileModeString := fmt.Sprintf("%v", file.Mode())
+		fileModeString += fmt.Sprintf(" %v", file.Size())
+		fileModeString += fmt.Sprintf(" %v ", file.ModTime().Format("2006/01/02 15:04"))
+		printString = fileModeString + filename
+	} else {
+		printString = filename
+	}
 	ext := filepath.Ext(filename)
 	switch ext {
 	case ".png":
-		color.Magenta(filename)
+		color.Magenta(printString)
 	case ".jpg":
-		color.Magenta(filename)
+		color.Magenta(printString)
 	case ".svg":
-		color.Magenta(filename)
+		color.Magenta(printString)
 	case ".mp4":
-		color.Magenta(filename)
+		color.Magenta(printString)
 	case ".mp3":
 		color.Magenta(filename)
 	case ".gz":
-		color.Red(filename)
+		color.Red(printString)
 	case ".zip":
-		color.Red(filename)
+		color.Red(printString)
 	default:
-		fmt.Println(filename)
+		fmt.Println(printString)
 	}
 }
 
 func fileModeCheck(file fs.FileInfo, filename string) bool {
+	printString := ""
+	if detailFlag {
+		//fileModeString := fmt.Sprintf("%v", file.Mode())
+		//printString = fileModeString + " " + filename
+		fileModeString := fmt.Sprintf("%v", file.Mode())
+		fileModeString += fmt.Sprintf(" %v", file.Size())
+		fileModeString += fmt.Sprintf(" %v ", file.ModTime().Format("2006/01/02 15:04"))
+		printString = fileModeString + filename
+	} else {
+		printString = filename
+	}
 	if file.IsDir() {
-		if checkGitBranch(filename) {
-			color.HiBlue(filename)
+		if checkGitBranch(file, filename) {
+			color.HiBlue(printString)
 		}
 	} else if file.Mode()&os.ModeSymlink == os.ModeSymlink {
-		color.Cyan(filename)
+		color.Cyan(printString)
 	} else if file.Mode()&0100 == 0100 {
-		color.Green(filename)
+		color.Green(printString)
 	} else {
 		return true
 	}
@@ -87,7 +123,7 @@ func dirwalk(dir string) []string {
 			filename = dir + "/" + file.Name()
 		}
 		if fileModeCheck(file, filename) {
-			checkPrint(filename)
+			checkExt(file, filename)
 		}
 	}
 	return paths
